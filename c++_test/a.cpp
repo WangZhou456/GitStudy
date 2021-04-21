9517349ec666c8206a19046087dbc87aacaa0185
wz <2135335609@qq.com>
2021-04-21 15:19:36 +0800

    modify a.cpp

--- c++_test/a.cpp
+++ c++_test/a.cpp
@@ -6,8 +6,9 @@ string s = "你好";
 string sub = s.substr(0, 3);
 cout << "s: " << s << " size: " << s.size() << endl;
 cout << "sub: " << sub << "       size: " << sub.size() << endl;
-char* cc = "你好";
-*cc = s.c_str();
+const char* cc = "你好";
+char* bb = cc;
+//*cc = s.c_str();
 cout << "cc: " << cc << "    size: " << sizeof(cc) << endl;
 return 0;
 }
